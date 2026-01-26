aee767ed077e0792bef09b556d077ca3472d93dc
Boris <bvozleev@gmail.com>
2026-01-26 15:13:55 +0300

    Тест 2

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from menu import MenuView
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 SCREEN_TITLE = "Coin Quest"
+
 if __name__ == "__main__":
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, fullscreen=True)
     arcade.set_background_color(arcade.color.AZURE)
